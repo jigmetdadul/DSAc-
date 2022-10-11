234b5ce929a3a52fceed0d14c900f48f9d899b80
jigmetdadul <jigmetdadul6@gmail.com>
2022-10-11 18:14:48 +0530

    update

--- operationOnArray/quickSrt.cpp
+++ operationOnArray/quickSrt.cpp
@@ -1,11 +1,22 @@
+//worst case: - n^2
+//average :- nlogn
 #include <iostream>
 using namespace::std;
 
 int partition(int*, int, int);
 void quickSrt(int*, int, int);
 void swap(int*, int*);
-int main(){
 
+int main(){
+    int arr[] = {7,5,42,44,13,5,1};
+    int N = sizeof(arr)/sizeof(int);
+    quickSrt(arr, 0, N-1);
+    for(int o = 0; o < N; o++){
+        cout<<arr[o]<<" ";
+    }
+    cout<<endl;
+    
+   
     return 0;
 }
 
@@ -14,6 +25,14 @@ void swap(int* a, int* b){
     *a = *b;
     *b = temp;
 }
+void quickSrt(int* arr, int lb, int ub){
+    if(lb < ub){
+        int loc = partition(arr, lb, ub);
+        quickSrt(arr, lb, loc - 1);
+        quickSrt(arr, loc+1, ub);
+    }
+}
+
 int partition(int* arr, int lb, int ub){
     int pivot = arr[lb];
     int start = lb;
@@ -23,7 +42,7 @@ int partition(int* arr, int lb, int ub){
         start++;
     }
     while(arr[end] > pivot){
-        start--;
+        end--;
     }
     if(start < end){
     swap(&arr[start], &arr[end]);
@@ -33,10 +52,3 @@ int partition(int* arr, int lb, int ub){
     return end;
 }
 
-void quickSrt(int* arr, int lb, int ub){
-    if(lb < ub){
-        int loc = partition(arr, lb, ub);
-        quickSrt(arr, lb, loc - 1);
-        quickSrt(arr, loc+1, ub);
-    }
-}
